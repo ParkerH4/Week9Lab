2e7c92bb5793ac282def8eb173ff6fcbe085e5b8
user <user@10.0.0.176>
2022-11-06 00:39:23 -0600

    Adjusted insert, update, delete.

--- src/java/dataaccess/DBUtil.java
+++ src/java/dataaccess/DBUtil.java
@@ -5,7 +5,9 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class DBUtil {
+   
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("UserPU");
+    
     public static EntityManagerFactory getEmFactory(){
     return emf;
     }
